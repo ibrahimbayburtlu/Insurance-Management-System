e3bf91c987f64f4e9dda35ba860826c3f3c50d70
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-02 13:19:06 +0300

    Insurance Management System

---
+++ InsuranceManagementSystem/src/User.java
@@ -0,0 +1,63 @@
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private final ArrayList<Address> addressList;
+    static private  final ArrayList<Insurance> InsuranceList =new ArrayList<>();
+    private final String name;
+    private final String surName;
+    private final String email;
+    private final String password;
+    private  final String job;
+    private final String age;
+    private Date lastLogin ;
+
+
+    public User(String name, String surName, String email, String password, String job, String age) {
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+        this.password = password;
+        this.job = job;
+        this.age = age;
+        lastLogin = new Date();
+        lastLogin = Date.from(Instant.now());
+    }
+
+    public ArrayList<Address> getAddressList() {
+        return addressList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin() {
+        lastLogin = Date.from(Instant.now());
+    }
+}
